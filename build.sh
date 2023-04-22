f8f5dccd156bc3645af2f4279ec0faeee1a80468
biechusheng233 <biechusheng233@gmail.com>
2023-04-22 19:47:45 +0800

    Init first

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# for rk3328 aarch64
+GCC_COMPILER=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf
+
+ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
+
+# build rockx
+BUILD_DIR=${ROOT_PWD}/build
+
+if [[ ! -d "${BUILD_DIR}" ]]; then
+  mkdir -p ${BUILD_DIR}
+fi
+
+cd ${BUILD_DIR}
+cmake .. \
+    -DCMAKE_C_COMPILER=${GCC_COMPILER}-gcc \
+    -DCMAKE_CXX_COMPILER=${GCC_COMPILER}-g++
+make -j4
+make install
+cd -
---
+++ build/CMakeFiles/paddle_lite_ocr.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alientek/ai_proj/paddle/paddle_lite_ocr/clipper.cpp" "/home/alientek/ai_proj/paddle/paddle_lite_ocr/build/CMakeFiles/paddle_lite_ocr.dir/clipper.cpp.o"
+  "/home/alientek/ai_proj/paddle/paddle_lite_ocr/cls_process.cc" "/home/alientek/ai_proj/paddle/paddle_lite_ocr/build/CMakeFiles/paddle_lite_ocr.dir/cls_process.cc.o"
+  "/home/alientek/ai_proj/paddle/paddle_lite_ocr/crnn_process.cc" "/home/alientek/ai_proj/paddle/paddle_lite_ocr/build/CMakeFiles/paddle_lite_ocr.dir/crnn_process.cc.o"
+  "/home/alientek/ai_proj/paddle/paddle_lite_ocr/db_post_process.cc" "/home/alientek/ai_proj/paddle/paddle_lite_ocr/build/CMakeFiles/paddle_lite_ocr.dir/db_post_process.cc.o"
+  "/home/alientek/ai_proj/paddle/paddle_lite_ocr/ocr_db_crnn.cc" "/home/alientek/ai_proj/paddle/paddle_lite_ocr/build/CMakeFiles/paddle_lite_ocr.dir/ocr_db_crnn.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../cxx/include"
+  "../cxx/opencv-linux-armv7hf/include"
+  "../cxx/opencv-linux-armv7hf/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
