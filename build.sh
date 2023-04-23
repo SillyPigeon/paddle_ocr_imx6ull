f4c0a1adaa9b1077ca0b069bb6d87a426a727c92
biechusheng233 <biechusheng233@gmail.com>
2023-04-23 15:39:16 +0800

    Fixed C++ compile-link error in gcc-4-9-4

--- build.sh
+++ build.sh
@@ -15,9 +15,9 @@ if [[ ! -d "${BUILD_DIR}" ]]; then
 fi
 
 cd ${BUILD_DIR}
-cmake .. \
-    -DCMAKE_C_COMPILER=${GCC_COMPILER}-gcc \
-    -DCMAKE_CXX_COMPILER=${GCC_COMPILER}-g++
+cmake .. #\
+    # -DCMAKE_C_COMPILER=${GCC_COMPILER}-gcc \
+    # -DCMAKE_CXX_COMPILER=${GCC_COMPILER}-g++
 make -j4
 make install
 cd -
--- model/run.sh
+++ model/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-./paddle_lite_ocr ch_ppocr_mobile_v2.0_det_slim_opt.nb ch_ppocr_mobile_v2.0_rec_slim_opt.nb ch_ppocr_mobile_v2.0_cls_slim_opt.nb ./lite_demo.png ppocr_keys_v1.txt
-
+./paddle_lite_ocr ./ch_ppocr_mobile_v2.0_det_slim_opt.nb ./ch_ppocr_mobile_v2.0_rec_slim_opt.nb ./ch_ppocr_mobile_v2.0_cls_slim_opt.nb ./lite_demo.png ppocr_keys_v1.txt
