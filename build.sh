2d5a0957cde783918cdd53822daa761f8c532259
biechusheng233 <biechusheng233@gmail.com>
2023-04-23 20:28:12 +0800

    Adjust compiler to arm-linux-gnueabihf-gcc v5.4

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # for your board compiler
-GCC_COMPILER=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf
+#GCC_COMPILER=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf
 
 ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
 
@@ -15,9 +15,9 @@ if [[ ! -d "${BUILD_DIR}" ]]; then
 fi
 
 cd ${BUILD_DIR}
-cmake .. \
-     -DCMAKE_C_COMPILER=${GCC_COMPILER}-gcc \
-     -DCMAKE_CXX_COMPILER=${GCC_COMPILER}-g++
+cmake .. #\
+     # -DCMAKE_C_COMPILER=${GCC_COMPILER}-gcc \
+     # -DCMAKE_CXX_COMPILER=${GCC_COMPILER}-g++
 make -j4
 make install
 cd -
