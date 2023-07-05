6a6c21df2ec80db4e2ef310b5ffd95d2c8f01f30
biechusheng233 <biechusheng233@gmail.com>
2023-07-05 13:48:45 +0800

    Add for opencv4

--- make.sh
+++ make.sh
@@ -5,6 +5,13 @@ if [ $1 == clean ]; then
 rm -rf ./build
 rm -rf ./install
 else
+# set the opencv lib version
+ocv_version="3"
+for par in $@; do
+    if [ ${par%=*} == "ocv_version" ]; then
+      ocv_version=${par#*=}
+    fi
+done
 # for your board compiler
 #GCC_COMPILER=/usr/local/arm/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf
 ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
@@ -17,7 +24,7 @@ if [[ ! -d "${BUILD_DIR}" ]]; then
 fi
 
 cd ${BUILD_DIR}
-cmake .. #\
+cmake .. -DOCV_LIB_VERSION=${ocv_version} #\
      # -DCMAKE_C_COMPILER=${GCC_COMPILER}-gcc \
      # -DCMAKE_CXX_COMPILER=${GCC_COMPILER}-g++
 make -j4
